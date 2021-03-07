cbcf7c30b9becec81885e863a120412ba9630fd4
Matt Randolph <mttrndlph@gmail.com>
2021-03-07 14:44:16 -0500

    Added initial files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Regular Hamburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Jalepeno Burger', true);
