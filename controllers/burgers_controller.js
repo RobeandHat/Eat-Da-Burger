9359c6552f0830e0427d17d9a30540a46456392d
Matt Randolph <mttrndlph@gmail.com>
2021-03-07 18:31:54 -0500

    fixed schema/seeds

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 router.post("/api/burgers", (req, res) => {
   burger.create(
     ["burger_name", "devoured"],
-    [req.body.name, req.body.sleepy],
+    [req.body.name, req.body.devoured],
     (result) => {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name, devoured) VALUES ('Double Cheese Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Regular Hamburger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Jalepeno Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Regular Hamburger');
+INSERT INTO burgers (name, devoured) VALUES ('Jalepeno Burger');
